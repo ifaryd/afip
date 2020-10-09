084e2f14d84cebbaf806b7fcf3066e8921a6cd6b
ifaryd <coulibalyzieisrael@gmail.com>
2020-10-09 00:39:54 +0000

    syncro

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('acceuil', views.index, name='home'),
+    path('', views.index, name='index'),
+    path('acceuil', views.index, name='index'),
     path('contact', views.contact, name='contact'),
     path('propos', views.propos, name='propos'),
     path('service', views.service, name='service'),
